ac827cb910f692f1af0df5d6828ee5bc56ea1062
kaushik1117 <potrukaushik11@gmail.com>
2024-11-10 09:23:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Donate from './components/Donate';
+import Investments from './components/Investments';
+import Dashboard from './components/Dashboard';
+import Start from './components/Start';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import CardDetails from './components/CardDetails';
+import PaymentPage from './components/PaymentPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/donate" element={<Donate />} />
+    <Route path="/investments" element={<Investments />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/start-fund" element={<Start />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/card/:id" element={<CardDetails />} /> 
+    <Route path="/payment/:id" element={<PaymentPage />} />
+    </Routes>
+    <Footer />
+    </Router>
+    </>
   );
 }
 
